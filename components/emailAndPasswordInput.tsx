f31539b777e514d884ef8aa91e6a0419f0b04ab6
Ethan Weilheimer <ethanweilheimer@Ethans-MacBook-Pro-2.local>
2023-03-03 12:30:54 -0600

    Email and Password base works

---
+++ components/emailAndPasswordInput.tsx
@@ -0,0 +1,101 @@
+import { useNavigation } from "@react-navigation/native"
+import React, { useState } from "react"
+import { View } from "react-native"
+import { SafeAreaInsetsContext } from "react-native-safe-area-context"
+import { Colors } from "react-native/Libraries/NewAppScreen"
+import { VerticallyCenteringRow, Spacer } from "../ui-base/layouts"
+import { Spacing } from "../ui-base/spacing"
+import { TextStyles } from "../ui-base/text"
+import BigButton from "./BigButton"
+import CancelButton from "./CancelButton"
+import TextField, { SignInField } from "./TextField"
+import { TextInput, Text, StyleSheet } from 'react-native'
+import { Radii } from '../ui-base/radii'
+
+
+export default function EmailAndPasswordInput(props: { forgotPassword: (email: string) => Promise<void>, onSubmit: (email: string, password: string) => Promise<void>, sent?:boolean, currentValues?: { email: string, password: string },error?:boolean, onCancel?: () => void }) {
+
+        const [email, setEmail] = useState(props.currentValues?.email ?? '')
+        const [password, setPassword] = useState(props.currentValues?.password ?? '')
+        const [isSubmitting, setIsSubmitting] = useState(false)
+        const safeAreaInsets = React.useContext(SafeAreaInsetsContext)
+    
+        const nameValid = email.length > 0
+        const iconValid = password.length > 6 && /\d/.test(password);
+    
+        const navigation = useNavigation()
+    
+        const cancel = () => {
+            if (props.onCancel) {
+                props.onCancel()
+            } else {
+                navigation.goBack()
+            }
+        }
+    
+        return (
+            <>
+                <View style={{ justifyContent: "flex-start", flex: props.currentValues ? 0 : 1 }}>
+                    <Text style={[TextStyles.h2, { marginBottom: Spacing.BigGap }]}>{ props.sent?"Reset Email was Sent":props.error?"There Was an Error Retry":'Enter Email and Password'}</Text>
+                    {/* <Text style={[TextStyles.p2, { marginVertical: Spacing.Gap }]}>{`ID: ${tagID}`}</Text> */}
+                    <SignInField
+                        inputType="email"
+                        placeholder='Email'
+                        value={email}
+                        onChangeText={(text) => {
+                            setEmail(text)
+                        }}
+                    />
+                    <SignInField
+                        inputType="password"
+                        placeholder='Password'
+                        value={password}
+                        onChangeText={(text) => {
+                            setPassword(text)
+                        }}
+                    />
+                
+                
+                    <View style={{height:"40%"}}>
+                    <BigButton 
+                        label={"Sign In"} 
+                        disabled={ ! nameValid || ! iconValid || (props.currentValues && (props.currentValues.email === email && props.currentValues.password === password))} 
+                        isLoading={isSubmitting}
+                        onPress={ async () => {
+                            setIsSubmitting(true)
+                            await props.onSubmit(email, password)
+                            setIsSubmitting(false)
+                        }}
+                    />
+                    <BigButton 
+                        label={"Forgot Password"} 
+                        disabled={ ! nameValid} 
+                        isLoading={isSubmitting}
+                        onPress={ async () => {
+                            setIsSubmitting(true)
+                            await props.forgotPassword(email)
+                            setIsSubmitting(false)
+                        }}
+                    />
+                    </View>
+                    </View>
+            </>
+        )
+    }
+    
+
+
+    const styles = StyleSheet.create({
+        addItemButton: {
+            paddingVertical: Spacing.Gap - 4,
+            paddingHorizontal: Spacing.Gap + 2,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: Colors.White,
+            borderRadius: 100,
+            flexShrink: 1, 
+            borderColor: Colors.White, 
+            borderWidth: 4
+        }
+    })
+    
--- screens/account/SignIn.tsx
+++ screens/account/SignIn.tsx
@@ -1,6 +1,6 @@
 import { GoogleSignin } from "@react-native-google-signin/google-signin"
 import auth from "@react-native-firebase/auth"
-import React from "react"
+import React, { useState } from "react"
 import {
     Text,
     View,
@@ -8,9 +8,11 @@ import {
 import { ScreenBase } from "../../ui-base/containers"
 import { TextStyles } from "../../ui-base/text"
 import BigButton from "../../components/BigButton"
+import EmailAndPasswordInput from "../../components/emailAndPasswordInput"
 
 export default function SignIn(props: { }) {
-
+    const [error,setError] = useState(false)
+    const [sent,setSent] = useState(false)
     async function onGoogleSignIn() {
         // Check if your device supports Google Play
         await GoogleSignin.hasPlayServices({
@@ -31,11 +33,35 @@ export default function SignIn(props: { }) {
             "199074098912-np9b1220ailpsbsn2ma5psoeabsm3pm9.apps.googleusercontent.com",
     })
 
+async function emailSignIn(email:string,password:string) {
+
+    try{
+await auth().createUserWithEmailAndPassword(email, password)
+    }
+    catch(err){
+        setError(true)
+    }
+
+}
+
+async function forgotPassword(email:string) {
+
+    try{
+await auth().sendPasswordResetEmail(email)
+setSent(true)
+    }
+    catch(err){
+        setError(true)
+    }
+
+}
+
     return (
         <ScreenBase>
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text style={TextStyles.h1}>Welcome!</Text>
             </View>
+            <EmailAndPasswordInput error ={error} sent={sent} forgotPassword={async (email)=>{await forgotPassword(email)}} onSubmit={ async (email,password)=>{await emailSignIn(email,password)}} />
             <BigButton
                 label='Sign in with Google'
                 onPress={() =>
