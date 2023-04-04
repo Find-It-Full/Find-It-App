b2ef46ee5a063ae0f2f74d9acac7cd8db902c869
Ethan Weilheimer <ethanweilheimer@Ethans-MacBook-Pro-2.local>
2023-04-04 12:11:28 -0500

    Initials fix

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -3,13 +3,15 @@ import { View, Text } from 'react-native'
 import { Colors } from '../ui-base/colors'
 import { TextStyles } from '../ui-base/text'
 import auth from '@react-native-firebase/auth'
+import items from '../reducers/items'
 
 export default function UserProfile() {
-
     const user = auth().currentUser ?? { displayName: '_ _' }
     const name = user.displayName ?? '_ _'
-    const initials = name.split(' ').map((str) => str[0]).reduce((prev, cur) => `${prev}${cur}`)
-
+    let initials = name.split(' ').map((str) => str[0]).reduce((prev, cur) => `${prev}${cur}`)
+    if (initials == "__"){
+        initials = "􀉩"
+    }
     return (
         <View style={{ justifyContent: 'center', alignItems: 'stretch', flexDirection: 'row' }}>
             <View style={{ width: 86, height: 86, backgroundColor: Colors.ButtonColor, borderRadius: 60, justifyContent: 'center', alignItems: 'center' }}>
