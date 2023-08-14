b1e6c132ef3cae2b9ebb9a3adea4962b85c3aa06
Thomas Urey <tsurey2002@gmail.com>
2023-08-14 17:15:13 -0400

    fixed item text overlapping other ui

--- components/items/ItemProfile.tsx
+++ components/items/ItemProfile.tsx
@@ -9,12 +9,12 @@ import { Shadows } from '../../ui-base/shadows';
 
 export default function ItemProfile({ icon, name }: Item) {
     return (
-        <VerticallyCenteringGroupedRow style={{ justifyContent: 'flex-start' }}>
+        <VerticallyCenteringGroupedRow style={{ justifyContent: 'flex-start', flexShrink: 1, paddingRight: Spacing.Gap * 2 }}>
             <ItemIconContainer>
                 <Text style={TextStyles.smallEmoji}>{icon}</Text>
             </ItemIconContainer>
             <Spacer size={Spacing.Gap} />
-            <Text style={TextStyles.h3}>{name}</Text>
+            <Text style={TextStyles.h3} ellipsizeMode='tail' numberOfLines={1} >{name}</Text>
         </VerticallyCenteringGroupedRow>
     )
 }
